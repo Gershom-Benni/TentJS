dc6bdf8a9c0ea96cc4f82e763c5249758087ddab
Gershom-Benni <gershombenni007@gmail.com>
2024-09-27 14:44:37 +0530

    added div function

--- src/core.js
+++ src/core.js
@@ -1,3 +1,11 @@
-const div = (textContent = "", attributes = {}) => {
-
-}
+const div = (child = "", attributes = {}) => {
+  const div = document.createElement("div");
+  if (typeof child === "string") {
+    div.textContent = child;
+  }
+  const entries = Object.entries(attributes);
+  entries.forEach(([key, value]) => {
+    div.setAttribute(key, value);
+  });
+  return div;
+};
